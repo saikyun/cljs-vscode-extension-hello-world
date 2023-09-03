c3c9f2580c160d174187c988893ab7ee1a98feab
Carlos Fontes <carlos@fragcolor.xyz>
2023-09-03 15:12:54 +0900

    Upgrade libs

--- src/extension/core.cljs
+++ src/extension/core.cljs
@@ -2,13 +2,16 @@
 
 (def vscode (js/require "vscode"))
 
-(defn activate
-  [context]
-  (.log js/console "Yeah boii")
-  (let [disposable (.. vscode.commands
-                      (registerCommand
-                      "extension.helloWorld"
-                      #(.. vscode.window (showInformationMessage "Hello World!"))))]
+(defn reload []
+  (.log js/console "Reloading...")
+  (js-delete js/require.cache (js/require.resolve "./extension")))
+
+(defn hella-world []
+  (.. vscode.window (showInformationMessage "Hella World!")))
+
+(defn activate [context]
+  (.log js/console "Activating")
+  (let [disposable (.. vscode.commands (registerCommand "extension.shards-lsp" #'hella-world))]
     (.. context.subscriptions (push disposable))))
 
 (defn deactivate [])
