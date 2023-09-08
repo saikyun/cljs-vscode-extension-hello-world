b28ea765dad006e2a34f4ed5d9abde585ea6450a
Carlos Fontes <carlos@fragcolor.xyz>
2023-09-09 00:02:26 +0900

    Improve perf in goto def lookup by calc wire locations AOT

--- src/extension/core.cljs
+++ src/extension/core.cljs
@@ -24,7 +24,39 @@
 
 (def json->clj (comp #(js->clj % :keywordize-keys true) js/JSON.parse))
 
-(defn ->ast []
+(defn ->location
+  [doc a-range]
+  #js {"range" a-range
+       "uri" (.-uri doc)})
+
+(defn ->range [{:keys [start-pos end-pos]}]
+  (new vscode/Range
+       (:line start-pos)
+       (:column start-pos)
+       (:line end-pos)
+       (:column end-pos)))
+
+(defn ->wires-ast-info [ast]
+  (some->> ast
+           :statements
+           (keep (comp :blocks :Pipeline))
+           (mapcat identity)
+           (filter #(= "wire" (some-> % :content :Func :name :name)))
+           (map #(hash-map :wire (some->> % :content :Func :params (keep (comp :name :Identifier :value)) (first))
+                           :line-info (:line_info %)))))
+
+(defn ->wire-location [doc {:keys [wire line-info]}] 
+  (let [{:keys [line column]} line-info]
+    {:wire wire
+     :location (->location doc
+                           (->range
+                            {:start-pos {:line (dec line) :column (+ column 5)}
+                             :end-pos {:line (dec line) :column (+ column 5 (count wire))}}))}))
+
+(defn ->wire-locations [doc ast]
+  (map (partial ->wire-location doc) (->wires-ast-info ast)))
+
+(defn ->ast [ast]
   (let [editor vscode/window.activeTextEditor
         doc (some-> editor .-document)
         text (some-> doc .getText)]
@@ -42,43 +74,18 @@
                  (println error b c)
                  (swap! ast assoc-in [doc.fileName :code-hash] (hash text))
                  (when (fs/existsSync rand-ast-path)
-                   (->> rand-ast-path slurp json->clj (swap! ast assoc-in [doc.fileName :ast]))
+                   (->> rand-ast-path slurp json->clj (->wire-locations doc) (swap! ast assoc-in [doc.fileName :wire-locations]))
                    (fs/unlink rand-ast-path (fn [err] (println err))))
                  (fs/unlink rand-shs-path (fn [err] (println err)))))))))
 
-(defn ->location
-  [doc a-range]
-  #js {"range" a-range
-       "uri" (.-uri doc)})
-
-(defn ->range [{:keys [start-pos end-pos]}]
-  (new vscode/Range 
-       (:line start-pos)
-       (:column start-pos)
-       (:line end-pos)
-       (:column end-pos)))
-
-(defn ->wire-pos
-  [ast wire-name]
-  (some->> ast
-           :statements
-           (keep (comp :blocks :Pipeline))
-           (mapcat identity)
-           (filter #(ffilter (fn [m] (= wire-name (some-> m :value :Identifier :name)))
-                             (-> % :content :Func :params)))
-           (ffilter #(= "wire" (some-> % :content :Func :name :name)))
-           :line_info))
-
 (defn handle-goto-def [ast]
   (fn [^js doc ^js pos _]
+    (println "handle-goto-def. ast" ast)
     (when doc
       (let [word-range (.getWordRangeAtPosition doc pos #"[a-z_][a-zA-Z0-9_.-]*")
             word (.getText doc word-range)
-            {:keys [line column]} (->wire-pos (get-in @ast [doc.fileName :ast]) word)]
-        (->location doc
-                    (->range
-                     {:start-pos {:line (dec line) :column (+ column 5)}
-                      :end-pos {:line (dec line) :column (+ column 5 (count word))}}))))))
+            wire-locations (get-in @ast [doc.fileName :wire-locations])]
+        (:location (ffilter #(= word (:wire %)) wire-locations))))))
 
 (defn reload []
   (.log js/console "Reloading...")
@@ -86,10 +93,10 @@
 
 (defn activate [context]
   (doto ^js (.-subscriptions context)
-    (.push (vscode/window.onDidChangeVisibleTextEditors (fn [_] (println "onDidChangeVisibleTextEditors") (->ast))))
-    (.push (vscode/window.onDidChangeActiveTextEditor (fn [_] (println "onDidChangeActiveTextEditor" (->ast)))))
-    (.push (vscode/window.onDidChangeWindowState (fn [_] (println "onDidChangeWindowState" (->ast)))))
-    (.push (vscode/workspace.onDidSaveTextDocument (fn [_] (println "onDidSaveTextDocument" (->ast)))))
+    (.push (vscode/window.onDidChangeVisibleTextEditors (fn [_] (println "onDidChangeVisibleTextEditors") (->ast ast))))
+    (.push (vscode/window.onDidChangeActiveTextEditor (fn [_] (println "onDidChangeActiveTextEditor" (->ast ast)))))
+    (.push (vscode/window.onDidChangeWindowState (fn [_] (println "onDidChangeWindowState" (->ast ast)))))
+    (.push (vscode/workspace.onDidSaveTextDocument (fn [_] (println "onDidSaveTextDocument" (->ast ast)))))
     (.push (vscode/languages.registerDefinitionProvider "shards" #js {:provideDefinition (handle-goto-def ast)}))))
 
 (defn deactivate [])
